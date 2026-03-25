50274266c8a900f27c81f59b44f2c4c32be97281
Sigge <sigge@hogvik-hansson.se>
2026-03-25 21:53:58 +0100

    git is such an intuitive piece of software

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MusicalShotgun
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Smonglaren.MusicalShotgun";
-        public const string PLUGIN_NAME = "MusicalShotgun";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
